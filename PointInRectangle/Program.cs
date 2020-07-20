e661a87eaf3c2783cf2d64d8b92b05df393f68f3
vinhhn3 <vinhhn3@fe.edu.vn>
2020-07-20 15:38:16 +0700

    Add Point in Rectangle Skeleton

---
+++ PointInRectangle/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PointInRectangle
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			Point point = new Point(2, 100);
+			Rectangle rectangle = new Rectangle(1, 4, 5, 1);
+			Console.WriteLine(rectangle.Contains(point));
+
+			Console.ReadLine();
+		}
+	}
+}
---
+++ PointInRectangle/Rectangle.cs
@@ -0,0 +1,28 @@
+﻿namespace PointInRectangle
+{
+	public class Rectangle
+	{
+		public Point TopLeft;
+		public Point BottomRight;
+
+		public Rectangle()
+		{
+			TopLeft = new Point();
+			BottomRight = new Point();
+		}
+
+		public Rectangle(int topLeftX, int topLeftY, int bottomRightX, int bottomRightY)
+		{
+			TopLeft = new Point(topLeftX, topLeftY);
+			BottomRight = new Point(bottomRightX, bottomRightY);
+		}
+
+		public bool Contains(Point point)
+		{
+			return TopLeft.X < point.X &&
+				point.X < BottomRight.X &&
+				BottomRight.Y < point.Y &&
+				point.Y < TopLeft.Y;
+		}
+	}
+}
