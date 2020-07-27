b56c1754a4c1f0f39a2321e02f074475b54d1547
vinhhn3 <vinhhn3@fe.edu.vn>
2020-07-27 16:36:36 +0700

    Lab 1

---
+++ EncapsulationLab/Person.cs
@@ -0,0 +1,57 @@
+﻿namespace EncapsulationLab
+{
+	internal class Person
+	{
+		private string _firstName;
+		private string _lastName;
+		private int _age;
+
+		public string FirstName
+		{
+			get
+			{
+				return _firstName + "AAAAXXXXX";
+			}
+			set
+			{
+				_lastName = value;
+			}
+		}
+
+		public string LastName
+		{
+			get
+			{
+				return _lastName + "JAHGKASJHKAJ";
+			}
+			set
+			{
+				_lastName = value;
+			}
+		}
+
+		public int Age
+		{
+			get
+			{
+				return _age;
+			}
+			set
+			{
+				_age = value;
+			}
+		}
+
+		public Person(string firstName, string lastName, int age)
+		{
+			_firstName = firstName;
+			_lastName = lastName;
+			_age = age;
+		}
+
+		public override string ToString()
+		{
+			return FirstName + " " + LastName + " " + Age;
+		}
+	}
+}
---
+++ EncapsulationLab/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EncapsulationLab
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			Person person = new Person("ABC", "XYZ", 10);
+			Console.WriteLine(person.ToString());
+
+			person.FirstName = "BBBB";
+
+			Console.WriteLine(person.ToString());
+
+			Console.ReadKey();
+		}
+	}
+}
