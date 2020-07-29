b820dca4c8fd79008e6f94453fb87921f78d4531
vinhhn3 <vinhhn3@fe.edu.vn>
2020-07-29 15:32:14 +0700

    Skeleton:  Lab 1

---
+++ Interfaces/Circle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Interfaces
+{
+	internal class Circle : IDrawable
+	{
+		private int _radius;
+
+		public Circle()
+		{
+		}
+
+		public Circle(int r)
+		{
+			_radius = r;
+		}
+
+		public void Draw()
+		{
+			// TO DO: Draw an circle with _radius
+			Console.WriteLine("Radius: " + _radius);
+		}
+	}
+}
---
+++ Interfaces/Rectangle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Interfaces
+{
+	internal class Rectangle : IDrawable
+	{
+		private int _height;
+		private int _width;
+
+		public Rectangle()
+		{
+		}
+
+		public Rectangle(int height, int width)
+		{
+			_height = height;
+			_width = width;
+		}
+
+		public void Draw()
+		{
+			// TO DO : draw an rectangle with _width and _height
+			Console.WriteLine();
+		}
+	}
+}
