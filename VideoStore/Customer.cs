0f17e097b22687c84317cbae4b4aedc5ae972cf5
vinhhn3 <vinhhn3@fe.edu.vn>
2020-08-05 13:51:55 +0700

    Add VideoStore project

---
+++ VideoStore/Customer.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+
+namespace VideoStore
+{
+	internal class Customer
+	{
+	}
+
+	internal class RentalInvoice
+	{
+		private Customer Customer;
+		private List<RentalItem> RentalItems;
+
+		public RentalInvoice(Customer customer)
+		{
+			Customer = customer;
+			RentalItems = new List<RentalItem>();
+		}
+
+		public void AddRentalItem(RentalItem item)
+		{
+			RentalItems.Add(item);
+		}
+	}
+
+	class CheckoutScreen
+	{
+		void ShowRentalInvoice(RentalInvoice invoice)
+		{
+
+		}
+	}
+
+	internal abstract class RentalItem
+	{
+	}
+
+	internal class DVD : RentalItem
+	{
+	}
+
+	internal class VHS : RentalItem
+	{
+	}
+
+	internal class Game : RentalItem
+	{
+	}
+}
